a7a64f4fe82ce3d61c79b3b9d77291e33cf7197c
diogotavc <diogotavc@protonmail.com>
2025-05-24 21:57:43 +0100

    messing with the map for fun

--- js/maps/level1_map.js
+++ js/maps/level1_map.js
@@ -12,15 +12,21 @@ export const mapData = {
     },
     layout: [ // Grid representing the map
         // Each element: [tileAssetName, rotationY_degrees] or null for empty
-        [null, ['road_straight', 0], ['building_a', 90]],
-        [['road_bend', 90], ['road_crossroad', 0], ['road_straight', 90]],
-        [['building_b', 0], ['road_straight', 0], null],
+        [null, null, ['road_straight', 90], ['building_a', 90]],
+        [null, null, ['road_straight', 90], null],
+        [['road_bend', 90], ['road_straight', 0], ['road_crossroad', 0], ['road_straight', 0]],
+        [['road_straight', 90], null, ['road_straight', 90], null],
+        [['road_straight', 90], null, ['road_straight', 90], null],
+        [['road_straight', 90], null, ['road_straight', 90], null],
+        [['building_b', 0], null, ['road_straight', 90], null],
+        [null, null, ['road_straight', 90], null],
     ],
-    tileSize: 12, // Assuming Kenney assets are roughly 4x4 units, adjust as needed
+    tileSize: 6, // Assuming Kenney assets are roughly 4x4 units, adjust as needed
     tileScale: { x: 6, y: 6, z: 6 }, // Added for scaling map assets
     startPoints: { // Define named start/finish points
-        'start1': { x: 1, z: 0, carRotationY: 0 }, // Grid coordinates
-        'finish1': { x: 1, z: 2, carRotationY: 90 },
+        //=11.05, Y=0.00, Z=-3.25
+        'start1': { x: 1.84, z: 0, carRotationY: 0 }, // Grid coordinates
+        'finish1': { x: 2.15, z: 7, carRotationY: 90 },
         'start_fireStation': { x: 0, z: 2, carRotationY: 180, yOffset: 0 }, // Example coordinates
         'finish_apartment': { x: 2, z: 0, carRotationY: 0, yOffset: 0 }
     }
