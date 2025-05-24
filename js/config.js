2a57b00be2c3fa76970587660d77b11009645870
diogotavc <diogotavc@protonmail.com>
2025-05-24 21:43:25 +0100

    set debugging flags to false

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-export const DEBUG_GENERAL = true;
-export const DEBUG_CAR_COORDS = true;
-export const DEBUG_COLLISIONS = true;
-export const DEBUG_MODEL_LOADING = true;
-export const DEBUG_MAP_LEVEL_LOGIC = true;
-export const DEBUG_REWIND = true;
+export const DEBUG_GENERAL = false;
+export const DEBUG_CAR_COORDS = false;
+export const DEBUG_COLLISIONS = false;
+export const DEBUG_MODEL_LOADING = false;
+export const DEBUG_MAP_LEVEL_LOGIC = false;
+export const DEBUG_REWIND = false;
